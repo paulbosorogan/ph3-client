a3b2e2f056b61b37df5453c36fcc56f6dcaf126a
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-28 20:26:44 -0400

    Update navigation links and buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,9 +9,9 @@ function Navigation(){
             <Navbar.Toggle aria-controls='navbarScroll' data-bs-target='#navbarScroll' />
                 <Navbar.Collapse id="navbarScroll">
                  <Nav>
-                    <NavLink eventKey='1' as={Link} to='/'>Home</NavLink>
-                    <NavLink eventKey='2' as={Link} to='/books'>Books</NavLink>
-                    <NavLink eventKey='3' as={Link} to='/add-new-book'>Add new book</NavLink>
+                    <NavLink eventkey='1' as={Link} to='/'>Home</NavLink>
+                    <NavLink eventkey='2' as={Link} to='/books'>Books</NavLink>
+                    <NavLink eventkey='3' as={Link} to='/new-book'>Add new book</NavLink>
                  </Nav>
                 </Navbar.Collapse>
             </Navbar>
