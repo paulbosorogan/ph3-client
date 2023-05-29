e2c0d101d5e7398a34ac2d554f677c4e9975a7c7
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-28 20:26:08 -0400

    Update code for delete function

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,21 @@
 import React from "react";
 import BookCard from "./BookCard";
 
-function Books(){
+
+
+function Books({books, onDeleteBook}){
+
+
     return(
-       <BookCard/>
+        <div className="wrapper">
+            <div className="book-list">
+                <h1>My book collection:</h1>
+            {books.map((book)=> (
+                <BookCard book={book} key={book.id} onDeleteBook={onDeleteBook} />
+                ))}
+            </div>
+           </div>
+
     )
 }
 
