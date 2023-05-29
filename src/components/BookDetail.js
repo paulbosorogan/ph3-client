e88ebbcc02a2580b397128a866dcc3fbbbf20f67
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-28 20:24:43 -0400

    Update layout and children components with information flow

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,32 +1,38 @@
-import React, {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import React, {useState} from "react";
+import { NavLink, useParams } from "react-router-dom";
 import Review from "./Review";
+import EditReview from "./EditReview"
 
-function BookDetail(){
-    const [bookInfo, setBookInfo] = useState(null)
-    const {id} = useParams
 
-    useEffect(()=>{
-        fetch(`http://localhost:9292/book/${id}`)
-        .then(r=>r.json())
-        .then(dataBook=>setBookInfo(dataBook))
-    },[id])
 
-    const {title, author, description, genre, reviews} = bookInfo
-    return (
-    <section>
-    <div>
-        <h1>{title}</h1>
-        <h1>{author}</h1>
-        <h1>{description}</h1>
-        <h1>{genre}</h1>
-        {reviews.map((review)=>{
-           <Review review={review} />
-        })}
-        
+function BookDetail({books, onUpdateRev}){
+  const [isEdit, setIsEdit] = useState(true)
+  const parameter = useParams()
+  const foundBook = books.find((individualBook) => individualBook.id === parseInt(parameter.id))
+
+  function onEdit(){
+    setIsEdit(isEdit=> !isEdit)
+  }
+ 
+return (
+    <div className="book-detail-card">
+    <NavLink to={`/books`}><p>Back to Books</p></NavLink> 
+        <img src={foundBook.image} alt='cover-book-detail'/>
+        <hr></hr>
+        <h3><b>"{foundBook.title}"</b></h3>
+        <div className="detail-info">
+        <p><b>Author:</b> {foundBook.author}</p>
+        </div>
+        <br></br>
+       {foundBook.reviews.map((review)=>{
+        return (
+          <div key={review.id}>
+            {isEdit? <Review review={review} onEdit={onEdit}/>
+             : <EditReview review={review} onUpdateRev={onUpdateRev} onEdit={onEdit}/>}
+          </div>
+       )})}
     </div>
-    </section>
-    )
+  )
 }
 
 export default BookDetail
