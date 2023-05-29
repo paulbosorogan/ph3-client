aae9524e9355f996c99fcd99a0c8761595c0b95d
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-28 20:23:57 -0400

    Update layout and components

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function BookCard(){
+function BookCard({
+    book:{id, title, author, image}, 
+    onDeleteBook}){
+  
+   function handleDelete(){
+    fetch(`http://localhost:9292/books/${id}`,{
+        method: "DELETE",
+    })
+    .then(onDeleteBook(id))
+    alert('Are you sure you want to delete the book?')
+   } 
+  
     return(
-        <>Book Card</>
+        
+        <div className="book-card">
+            
+            <img src={image} alt='book-cover'/>
+            <hr></hr>
+        <div className="book-info">
+            <h4>"{title}"</h4>
+            <p>By: <b>{author}</b></p>
+            <div className="actions">
+        <NavLink to={`/books/${id}`}><p>See details</p></NavLink> 
+        <button className='trash-bttn'><span role="img" aria-label="delete" onClick={handleDelete}>🗑</span></button>
+          </div>
+         </div>
+        </div>
     )
 }
-
 export default BookCard
