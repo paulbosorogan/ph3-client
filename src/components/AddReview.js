3f22dd4c0a4cfbeaf61187c7c93ad2bda79b2664
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-01 14:42:15 -0400

    Update components

---
+++ src/components/AddReview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddReview({review, onEdit}){
+    return(
+        <div key={review.id} className="detail-info">
+          <p><b>Thoughts:</b> {review.comment}</p>
+           <button onClick={onEdit} className="edit-bttn"><span role="img" aria-label="edit">✏️</span></button>
+          </div>
+    )
+}
+
+export default AddReview
