be5bebca9f2223a1d11cbba826432d62e54355b3
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-02 17:30:45 -0400

    Update code

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -4,24 +4,16 @@ import { useNavigate } from "react-router-dom";
 function NewBook({addBook}){
     const [newTitle, setNewTitle] = useState("")
     const [newAuthor, setNewAuthor] = useState("")
-    const [newDescription, setNewDescription] = useState("")
-    const [newGenre, setNewGenre] = useState("")
     const [newCover, setNewCover] = useState("")
     const navigate = useNavigate();
 
-    function handleSubmitNB(e){
+    function handleSubmitNewBook(e){
         e.preventDefault()
 
         const bookData = {
             title: newTitle,
             author: newAuthor,
-            description: newDescription,
-            genre: newGenre,
-            image: newCover,
-            reviews: {
-                score: 0,
-                comment: 'Fill the blank',
-            }
+            image: newCover
         }
         
         fetch(`http://localhost:9292/books`, {
@@ -34,21 +26,16 @@ function NewBook({addBook}){
         .then(r=>r.json())
         .then(newBook => addBook(newBook))
         .then(()=> navigate('/books'))
-        console.log(bookData)
     }
     return(
         <div className="new-book-container">
         <h1>What book do you wanna read next?</h1>
-        <form className="new-book-form" onSubmit={handleSubmitNB}>
-            <label htmlFor="text">Book title:</label>
+        <form className="new-book-form" onSubmit={handleSubmitNewBook}>
+            <label className="new-book" htmlFor="text">Book title:</label>
             <input id="title" type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)}/>
-            <label htmlFor="text">Book author:</label>
+            <label className="new-book" htmlFor="text">Book author:</label>
             <input id="author" type="text" value={newAuthor} onChange={(e)=> setNewAuthor(e.target.value)}/>
-            <label htmlFor="text">Book description:</label>
-            <input id="description" type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)}/>
-            <label htmlFor="text">Book genre:</label>
-            <input id="genre" type="text" value={newGenre} onChange={(e)=>setNewGenre(e.target.value)}/>
-            <label htmlFor="text">Book cover:</label>
+            <label className="new-book" htmlFor="text">Book cover:</label>
             <input id="image_url" type="text" value={newCover} onChange={(e)=>setNewCover(e.target.value)}/>
             <input type="submit" value='Submit book'/>
         </form>
