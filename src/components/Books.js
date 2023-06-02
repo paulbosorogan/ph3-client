4edcdf93c6d7962efbb08aa504b32c43bf554d1c
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-02 17:29:21 -0400

    Update info

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,13 +3,11 @@ import BookCard from "./BookCard";
 
 
 
+
 function Books({books, onDeleteBook}){
-
-
     return(
         <div className="wrapper">
             <div className="book-list">
-                <h1>My book collection:</h1>
             {books.map((book)=> (
                 <BookCard book={book} key={book.id} onDeleteBook={onDeleteBook} />
                 ))}
