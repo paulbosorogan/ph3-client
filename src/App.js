b304f8bbbbcaf80e82259fa9bd5d616688db869f
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-02 17:29:01 -0400

    Update structure and code for state reloads

--- src/App.js
+++ src/App.js
@@ -4,50 +4,80 @@ import Home from './components/Home'
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Navigation from "./components/Navigation";
-import BookDetail from "./components/BookDetail";
+import NoteContainer from "./components/NoteContainer";
 
 
 function App() {
   const [books, setBooks] = useState([])
- 
+
   useEffect(()=>{
     fetch('http://localhost:9292/books')
     .then((r)=>r.json())
     .then((data)=>setBooks(data))
   },[])
 
+//new book 
 function addBook(newEntry){
   setBooks([...books, newEntry])
-
 }
+//delete book
 function onDeleteBook(id){
-  const updatedBooks = books.filter((bookUp)=> bookUp.id !== id)
-  setBooks(updatedBooks)
+  const afterBookDelete = books.filter((book)=> book.id !== id)
+  setBooks(afterBookDelete)
   }
-
-  function onUpdateRev(updatedRev){
-   const updatedReview = books.map((book) => {
-     if(book.id === updatedRev.book_id){
-      const filtered = book.reviews.filter((review) =>review.id !== updatedRev.id)
+//update note
+function onUpdateNote(updatedNote){
+  const updateNote = books.map((book)=>{
+    if(book.id === updatedNote.book_id){
+      const filteredNotes = book.notes.filter((note)=> note.id !== updatedNote.id)
       return {
         ...book,
-        reviews: [updatedRev, ...filtered]
+        notes: [updatedNote, ...filteredNotes]
       }
     } else {
       return book
     }
-   })
-   setBooks(updatedReview)
-  }
+  })
+  setBooks(updateNote)
+}
+//add note 
+function onAddNote(newNoteUpload){
+  const newNoteUploaded = books.map((book)=>{
+    if(book.id === newNoteUpload.book_id){
+      return {
+        ...book,
+        notes: [newNoteUpload, ...book.notes]
+      }
+    } else {
+      return book
+    }
+  })
+  setBooks(newNoteUploaded)
+}
 
-  
+//delete note
+function onDeleteNote(deletedNote){
+  const afterDeleteNote = books.map((book)=>{
+    if(book.id === deletedNote.book_id){
+      const filterDeleteNote = book.notes.filter((note)=> note.id !== deletedNote.id)
+      return {
+        ...book,
+        notes: filterDeleteNote
+      }
+    } else {
+      return book
+    }
+  })
+  setBooks(afterDeleteNote)
+}
   return (
    <div>
     <Navigation/>
       <Routes>
         <Route exact path='/' element={<Home/>} />
         <Route exact path='/books' element={<Books books={books} onDeleteBook={onDeleteBook}/>} />
-        <Route exact path='/books/:id' element={<BookDetail books={books} onUpdateRev={onUpdateRev}/>} />
+        <Route exact path='/books/:id' element={<NoteContainer books={books} onUpdateNote={onUpdateNote} onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}/>} />
         <Route exact path='/new-book' element={<NewBook addBook={addBook}/>} />
       </Routes>
    </div>
