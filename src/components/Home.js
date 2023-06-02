cfca12cb8b7dde2b1fb221110c203cfc09c0af50
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-02 17:29:48 -0400

    Changed code by accident, reverted to normal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
  
 function Home(){
+ 
+
     return(
     <div className="App">
         <header className="App-header">
-        <h1><b>in my reader's opinion</b></h1>
+        <h1><em>booknotes</em></h1>
           <div className='container'>
-            <p>the application that let's you review the books you've read</p>
-            <p><b>'Because reading is what? Fundamental'</b></p><em>RuPaul</em>
-            
+            <div className="container-info">
+            <p>welcome to booknotes</p>
+            <p>leave notes on your favorite books</p>
+            </div>
           </div>
         </header>
     </div>
